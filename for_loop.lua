e6badf11109ab252ab3bc0df6d4c16157d4d7ac5
sal'e chocho <mchocho@student.wethinkcode.co.za>
2021-07-03 22:10:52 +0100

    For loop statements

---
+++ for_loop.lua
@@ -0,0 +1,19 @@
+--[[
+  For loop statement synyax
+  for Init, max/min value, increment/decrement
+  do
+    Statments
+    end
+]]--
+
+for i = 10, 0, -1
+do
+  print("Value of i:", i)
+  end
+
+print("\nLet's try another one\n\n")
+
+for j = 0, 10, 1
+do
+  print("Value of j:", j)
+  end
