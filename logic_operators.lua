7b7b81c5d11f2024966bf69d55ab47baea1d3052
$al'e <mchocho@student.wethinkcode.co.za>
2021-07-03 22:40:53 +0200

    Logical operators

---
+++ logic_operators.lua
@@ -0,0 +1,14 @@
+print("Please enter a value:")
+a = tonumber(io.read())
+
+if a < 100 and a > 1 then
+  print("yes")
+  end
+
+print("Cool please enter another number:")
+b = tonumber(io.read())
+sum = a - b
+
+if sum == 4 or sum == 5 then
+  print("Yebo")
+  end
