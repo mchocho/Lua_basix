1617ca4367d2945fb5ad51d1974ce5c9b5d5369b
$al'e <mchocho@student.wethinkcode.co.za>
2021-07-03 21:18:45 +0200

    Arithmetic operators

---
+++ arithmetic_operatos.lua
@@ -0,0 +1,15 @@
+number1 = 42
+number2 = 2
+
+print("first number:", number1)
+print("second number:", number2)
+
+sum = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+quo = number1 / number2
+
+print("Sum of numbers:", sum)
+print("Difference of numbers:", diff)
+print("Product of numbers:", product)
+print("Quotent of numbers:", quo)
